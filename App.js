c716d1b572f4cd09b37d462b849364c470e5bd0e
jenarhanes <jenarhanes@gmail.com>
2023-08-12 11:35:17 -0700

    initial app commit

---
+++ App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <div id="background-wrap">
+          <div class="x1">
+              <div class="cloud"></div>
+          </div>
+
+          <div class="x2">
+              <div class="cloud"></div>
+          </div>
+
+          <div class="x3">
+              <div class="cloud"></div>
+          </div>
+
+          <div class="x4">
+              <div class="cloud"></div>
+          </div>
+
+          <div class="x5">
+              <div class="cloud"></div>
+          </div>
+        </div>
+        <div class="frame">
+  
+        </div>
+        <div>
+        <audio
+        controls>
+          <source
+          src="lofi-study-112191.mp3" 
+          type="audio/mpeg">
+          </source>
+        </audio>
+        </div>
+
+      </header>
+      
+    </div>
+    
+  );
+}
+
+export default App;
